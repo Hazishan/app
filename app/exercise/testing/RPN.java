32a398495b3aed4ae14407627414381214edf2b5
Lasse <lasse.wolske@stud.uni-goettingen.de>
2022-05-16 19:32:31 +0200

    RPN push

---
+++ app/exercise/testing/RPN.java
@@ -0,0 +1,46 @@
+package app.exercise.testing;
+import app.exercise.algebra.Rational;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+RPN = Reverse Polish Notation
+@link {https://en.wikipedia.org/wiki/Reverse_Polish_notation}
+test class for @see BasisFraction
+uses
+ */
+
+public class RPN {
+    public static void main(String[] args) {
+        String rpnString = "12*4/5+45+-";
+        Stack<Rational> numbers = new Stack();
+
+
+        for (int i = 0; i <= rpnString.length() - 1; i++) {
+            if (rpnString.charAt(i) != '+' && rpnString.charAt(i) != '-' && rpnString.charAt(i) != '*' && rpnString.charAt(i) != '/') {
+                long currentVal = Character.getNumericValue(rpnString.charAt(i));
+                numbers.push(new Rational(currentVal, 1));
+            } else{
+                try {
+                    Rational operantOne = numbers.pop();
+                    Rational operantTwo = numbers.pop();
+                    char operator = rpnString.charAt(i);
+                    switch (operator) {
+                        case '+' -> operantTwo.add(operantOne);
+                        case '-' -> operantTwo.sub(operantOne);
+                        case '*' -> operantTwo.mul(operantOne);
+                        case '/' -> operantTwo.div(operantOne);
+                    }
+                    numbers.push(operantTwo);
+                } catch (EmptyStackException e) {
+                    System.out.println("ERROR: Invalid amount of operators/operands");
+                    System.out.println(e.getClass());
+                    break;
+                }
+            }
+        }
+        System.out.println("solution: " + numbers.pop());
+    }
+}
+
